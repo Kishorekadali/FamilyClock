dce0bea2de41756ef2f08c8ab549352365015a77
Kishorekadali <kishorekadali2957@gmail.com>
2024-12-22 13:42:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState,useEffect } from "react";
+import Clock from './FamilyClock.jsx';
+import './App.css';
+import familypic from './assets/familypic.jpg';
+
+function App(){
+
+  return(
+    <>
+    
+    <Clock/>
+    </>
+  )
+};
+
+export default App
